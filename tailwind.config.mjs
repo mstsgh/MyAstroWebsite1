128776fe9cb0a2447124ca4b17afc29915610185
Rettungsdienstblog <steinberg.mirko@web.de>
2023-10-23 22:32:58 +0200

    2

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: "class",
 	theme: {
 		extend: {
 			colors: {
 				light: "#f5f5f5",
 				dark: "#1b1b1b",
+				grey: "#6C7B8B",
 			},
 		},
 	},
